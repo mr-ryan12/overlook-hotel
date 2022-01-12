40eef65910558f2d9cf8a420932828d499103776
Ryan McBride <mr.ryan444@gmail.com>
2022-01-12 16:44:39 -0700

    Add tests to user-test.js

--- test/user-test.js
+++ test/user-test.js
@@ -2,9 +2,23 @@ import chai from 'chai';
 const expect = chai.expect;
 import users from './sample-test-data/user-test-data';
 
-describe('See if the tests are running', function() {
-  it('should return true', function() {
-    expect(true).to.equal(true);
+describe('User', () => {
+
+  let user;
+  it.skip('should be a function', () => {
+    expect(User).to.be.a('function');
+  });
+
+  it.skip('should be an instance of user', () => {
+    expect(user).to.be.an.instanceof(User);
+  });
+
+  it.skip('should have an id', () => {
+    expect(user[0].id).to.equal(1);
+  });
+
+  it.skip('should have a name', () => {
+    expect(user[0].name).to.equal('Leatha Ullrich');
   });
 });
 
