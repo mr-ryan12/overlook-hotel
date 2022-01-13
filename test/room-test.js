683ef13d045996cbffe10ad682dcb486702947b6
Ryan McBride <mr.ryan444@gmail.com>
2022-01-13 15:31:08 -0700

    Pass room tests

--- test/room-test.js
+++ test/room-test.js
@@ -1,7 +1,7 @@
 import chai from 'chai';
 const expect = chai.expect;
 import rooms from './sample-test-data/rooms-test-data';
-import Room from '../src/classes/Booking';
+import Room from '../src/classes/Room';
 
 describe('Room', () => {
 
@@ -16,7 +16,7 @@ describe('Room', () => {
     expect(Room).to.be.a('function');
   });
 
-  it('should be an instance of Booking', () => {
+  it('should be an instance of Room', () => {
     expect(room).to.be.an.instanceof(Room);
   });
 
@@ -24,23 +24,23 @@ describe('Room', () => {
     expect(room.number).to.equal(12);
   });
 
-  it.skip('should have a room type', () => {
+  it('should have a room type', () => {
     expect(room.roomType).to.equal('single room');
   });
 
-  it.skip('should have the option to include a bidet', () => {
+  it('should have the option to include a bidet', () => {
     expect(room.bidet).to.equal(false);
   });
 
-  it.skip('should have a bed size', () => {
+  it('should have a bed size', () => {
     expect(room.bedSize).to.equal('twin');
   });
 
-  it.skip('should have a number of beds', () => {
+  it('should have a number of beds', () => {
     expect(room.numBeds).to.equal(2);
   });
 
-  it.skip('should have a cost per night', () => {
+  it('should have a cost per night', () => {
     expect(room.costPerNight).to.equal(172.09);
   });
 });
