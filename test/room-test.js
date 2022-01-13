10b1e3d691ef9475e9ce3a0d042cd514e80d421e
Ryan McBride <mr.ryan444@gmail.com>
2022-01-13 15:22:46 -0700

    Fix room-test.js file

--- test/room-test.js
+++ test/room-test.js
@@ -3,7 +3,7 @@ const expect = chai.expect;
 import rooms from './sample-test-data/rooms-test-data';
 import Room from '../src/classes/Booking';
 
-describe('Booking', () => {
+describe('Room', () => {
 
   let room;
   let room1 = rooms[0];
@@ -12,35 +12,35 @@ describe('Booking', () => {
     room = new Room(room1);
   });
 
-  it.skip('should be a function', () => {
+  it('should be a function', () => {
     expect(Room).to.be.a('function');
   });
 
-  it.skip('should be an instance of Booking', () => {
+  it('should be an instance of Booking', () => {
     expect(room).to.be.an.instanceof(Room);
   });
 
-  it.skip('should have a room number', () => {
-    expect(room1.number).to.equal(12);
+  it('should have a room number', () => {
+    expect(room.number).to.equal(12);
   });
 
   it.skip('should have a room type', () => {
-    expect(room1.roomType).to.equal('single room');
+    expect(room.roomType).to.equal('single room');
   });
 
   it.skip('should have the option to include a bidet', () => {
-    expect(room1.bidet).to.equal(false);
+    expect(room.bidet).to.equal(false);
   });
 
   it.skip('should have a bed size', () => {
-    expect(room1.bedSize).to.equal('twin');
+    expect(room.bedSize).to.equal('twin');
   });
 
   it.skip('should have a number of beds', () => {
-    expect(room1.numBeds).to.equal(2);
+    expect(room.numBeds).to.equal(2);
   });
 
   it.skip('should have a cost per night', () => {
-    expect(room1.costPerNight).to.equal(172.09);
+    expect(room.costPerNight).to.equal(172.09);
   });
 });
