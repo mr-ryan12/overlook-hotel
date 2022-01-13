b4ba9778c85beeaabd358f7d6729014ca9399650
Ryan McBride <mr.ryan444@gmail.com>
2022-01-13 15:00:13 -0700

    Pass booking tests

--- test/booking-test.js
+++ test/booking-test.js
@@ -5,7 +5,7 @@ import bookings from './sample-test-data/bookings-test-data';
 import rooms from './sample-test-data/rooms-test-data';
 import Booking from '../src/classes/Booking';
 
-describe('User', () => {
+describe('Booking', () => {
 
   let booking;
   let booking1 = bookings[3];
@@ -14,31 +14,31 @@ describe('User', () => {
     booking = new Booking(booking1);
   });
 
-  it.skip('should be a function', () => {
+  it('should be a function', () => {
     expect(Booking).to.be.a('function');
   });
 
-  it.skip('should be an instance of User', () => {
+  it('should be an instance of Booking', () => {
     expect(booking).to.be.an.instanceof(Booking);
   });
 
-  it.skip('should have an id', () => {
+  it('should have an id', () => {
     expect(booking.id).to.equal('5fwrgu4i7k55hl792');
   });
 
-  it.skip('should have a user id', () => {
+  it('should have a user id', () => {
     expect(booking.userID).to.equal(9);
   });
 
-  it.skip('should have a date', () => {
+  it('should have a date', () => {
     expect(booking.date).to.equal('2022/01/10');
   });
 
-  it.skip('should have a room number', () => {
+  it('should have a room number', () => {
     expect(booking.roomNumber).to.equal(19);
   });
 
-  it.skip('should not have any room service charges', () => {
+  it('should not have any room service charges', () => {
     expect(booking.roomServiceCharges).to.deep.equal([]);
   });
 });
